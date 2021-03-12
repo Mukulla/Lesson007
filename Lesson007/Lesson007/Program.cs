e50fc0af0cb973099fae05ebb9f8876e8c287438
Maxim <mbzozn@mail.ru>
2021-03-12 16:20:48 +0300

    New Proj Created

---
+++ Lesson007/Lesson007/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] SomeArray = new int[15];
+
+            SetRandomValues(SomeArray, -31, 31);
+            Show(SomeArray);
+
+            Console.Read();
+        }
+
+        static void SetRandomValues(int []SomeArray001, int Min, int Max)
+        {
+            if(!CheckArray(SomeArray001))
+            {
+                return;
+            }
+            Random Rand001 = new Random();           
+
+            for (int i = 0; i < SomeArray001.Length; ++i)
+            {
+                SomeArray001[i] = Rand001.Next(Min, Max);
+            }
+        }
+
+        static void Show(int[] SomeArray001)
+        {
+            if (!CheckArray(SomeArray001))
+            {
+                return;
+            }
+
+            foreach (var Item in SomeArray001)
+            {
+                Console.WriteLine(Item);
+            }
+        }
+
+        static bool CheckArray<T>( T[] SomeArray001)
+        {
+            if (SomeArray001 == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
